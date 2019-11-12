4a8bcc1827a299677d972464df89754929deea02
silas@2019 <mr.guosl@qq.com>
2019-11-12 16:48:15 +0800

    获取本机ip

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const request = require('./util/request')
 const packageJSON = require('./package.json')
 const exec = require('child_process').exec
 const cache = require('apicache').middleware
+const { getIPAdress } = require('./util')
 // version check
 exec('npm info NeteaseCloudMusicApi version', (err, stdout, stderr) => {
-  if(!err){
+  if (!err) {
     let version = stdout.trim()
-    if(packageJSON.version < version){
+    if (packageJSON.version < version) {
       console.log(`最新版本: ${version}, 当前版本: ${packageJSON.version}, 请及时更新`)
     }
   }
@@ -20,7 +21,7 @@ const app = express()
 
 // CORS & Preflight request
 app.use((req, res, next) => {
-  if(req.path !== '/' && !req.path.includes('.')){
+  if (req.path !== '/' && !req.path.includes('.')) {
     res.set({
       'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Origin': req.headers.origin || '*',
@@ -36,7 +37,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   req.cookies = {}, (req.headers.cookie || '').split(/\s*;\s*/).forEach(pair => {
     let crack = pair.indexOf('=')
-    if(crack < 1 || crack == pair.length - 1) return
+    if (crack < 1 || crack == pair.length - 1) return
     req.cookies[decodeURIComponent(pair.slice(0, crack)).trim()] = decodeURIComponent(pair.slice(crack + 1)).trim()
   })
   next()
@@ -44,7 +45,7 @@ app.use((req, res, next) => {
 
 // body parser
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // cache
 app.use(cache('2 minutes', ((req, res) => res.statusCode === 200)))
@@ -60,12 +61,12 @@ const special = {
 }
 
 fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
-  if(!file.endsWith('.js')) return
+  if (!file.endsWith('.js')) return
   let route = (file in special) ? special[file] : '/' + file.replace(/\.js$/i, '').replace(/_/g, '/')
   let question = require(path.join(__dirname, 'module', file))
 
   app.use(route, (req, res) => {
-    let query = Object.assign({}, req.query, req.body, {cookie: req.cookies})
+    let query = Object.assign({}, req.query, req.body, { cookie: req.cookies })
     question(query, request)
       .then(answer => {
         console.log('[OK]', decodeURIComponent(req.originalUrl))
@@ -74,7 +75,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
       })
       .catch(answer => {
         console.log('[ERR]', decodeURIComponent(req.originalUrl))
-        if(answer.body.code == '301') answer.body.msg = '需要登录'
+        if (answer.body.code == '301') answer.body.msg = '需要登录'
         res.append('Set-Cookie', answer.cookie)
         res.status(answer.status).send(answer.body)
       })
@@ -82,7 +83,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
 })
 
 const port = process.env.PORT || 3000
-const host = process.env.HOST || 'localhost'
+const host = process.env.HOST || getIPAdress() || 'localhost'
 
 app.server = app.listen(port, host, () => {
   console.log(`server running @ http://${host}:${port}`)
--- util/index.js
+++ util/index.js
@@ -2,5 +2,17 @@ module.exports = {
   toBoolean(val) {
     if (val === '') return val
     return val === 'true' || val == '1'
+  },
+  getIPAdress() {
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+          return alias.address;
+        }
+      }
+    }
   }
 }
